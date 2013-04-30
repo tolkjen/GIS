a3d4f69b6b02d993b373a7c7d6a13e077f80ec33
Tomasz Figa <tomasz.figa@gmail.com>
2013-05-01 00:29:19 +0200

    main: Implement most of the node disjoint shorthest pair algorithm
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>

--- GIS/main.cpp
+++ GIS/main.cpp
@@ -2,25 +2,105 @@
 #include <string>
 #include "BaseGraph.h"
 #include "GraphOperations.h"
+#include <list>
+#include <fstream>
 
 using namespace std;
 using namespace GraphOperations;
 
 // --------------------------------------------------------------------------
-string pathToString(vector<int> &path) {
-	if (path.empty()) {
-		return string("None");
-	}
+string pathToString(list<int> &path)
+{
+	if (path.empty())
+		return " -> ";
 
 	string result;
-	for (int i = 0; i < path.size() - 1; i++) {
-		result += to_string(path[i]) + " -> ";
+	result += " -> ";
+	for (int v : path)
+		result += to_string(v) + " -> ";
+	return result;
+}
+
+static void print(BaseGraph &graph, const char *filename)
+{
+	ofstream out(filename);
+
+	graph.print(out);
+}
+
+static int shortestPair(BaseGraph graph, int from, int to)
+{
+	// find shorthest path
+	SearchResult path = dijkstra(graph, from, to);
+	if (path.length < 0) {
+		cerr << "Failed to find path from " << from << " to " << to << endl;
+		return 1;
 	}
-	return result + to_string(path[path.size() - 1]);
+
+	cout << from << pathToString(path.path).c_str() << to << endl;
+	cout << "Length: " << path.length << endl;
+
+	print(graph, "graph1a.dot");
+
+	// transform the graph
+	int prev = from;
+	for (int v : path.path) {
+		graph.reverseEdge(prev, v);
+		prev = v;
+	}
+	graph.reverseEdge(prev, to);
+
+	print(graph, "graph1b.dot");
+
+	list<pair<int, int>> clones;
+
+	prev = from;
+	int prev2 = path.path.front();
+	for (int v : path.path) {
+		int v2 = graph.splitVertex(v);
+		graph.addEdge(v2, v, 0);
+		graph.removeEdge(v2, prev);
+		graph.removeEdge(prev2, v);
+		clones.emplace_front(v, v2);
+		prev = v;
+		prev2 = v2;
+	}
+	graph.removeEdge(prev2, to);
+
+	print(graph, "graph1c.dot");
+
+	// find shorthest pair
+	SearchResult path2 = dijkstra_modified(graph, from, to);
+	if (path2.length < 0) {
+		cerr << "Failed to find path from " << from << " to " << to << endl;
+		return 1;
+	}
+
+	cout << from << pathToString(path2.path).c_str() << to << endl;
+	cout << "Length: " << path2.length << endl;
+
+	// normalize the graph
+	for (auto &v : clones)
+		graph.mergeVertices(v.first, v.second);
+
+	prev = from;
+	for (int v : path.path) {
+		graph.reverseEdge(prev, v);
+		prev = v;
+	}
+
+	// calculate final paths
+	// TODO
+
+	return 0;
 }
 
 // --------------------------------------------------------------------------
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	int from = 0;
+	int to = 0;
+
 	// Check argument number
 	if (argc != 2) {
 		cerr << "Wrong usage!" << endl;
@@ -38,10 +118,8 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	// perform algo
-	SearchResult pathInfo = dijkstra_modified(graph, 0, graph.verticesCount() - 1);
-	cout << pathToString(pathInfo.path).c_str() << endl;
-	cout << "Length: " << pathInfo.length << endl;
+	while (scanf("%d %d", &from, &to) == 2)
+		shortestPair(graph, from, to);
 
 	getchar();
 	return 0;
